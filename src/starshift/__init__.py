4416c688211eb2f5d52008693074108c047493e5
wdc <wdc23a@acu.edu>
2025-12-14 22:20:23 -0600

    Removed old code

--- src/starshift/__init__.py
+++
@@ -1,8 +0,0 @@
-# Errors
-from .star_shift import ShiftError, ShiftConfigError, ShiftValidationError
-# Config
-from .star_shift import ShiftConfig, DEFAULT_SHIFT_CONFIG
-# Decorators
-from .star_shift import shift_validator, shift_setter, shift_repr, has_serializer, shift_serializer
-# Shift
-from .star_shift import Shift
--- test/manual.py
+++ test/manual.py
@@ -1,19 +1,9 @@
-from typing import Any, Optional, Union, Callable, get_type_hints
 from starshift import *
-
-DEFAULT_SHIFT_CONFIG.verbosity = 0
 
 
 
 def test():
-    class Test(Shift):
-        val: int
-
-        @shift_validator('val')
-        def validate_val(self, data: dict[str, Any], field) -> bool:
-            return True
-
-    test = Test(val=10)
+    """Test something"""
 
 if __name__ == '__main__':
     test()
